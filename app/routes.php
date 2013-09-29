0511d204298e68edc559d40d80dcd8d5a6146fdc
Naor Ami <naoric@gmail.com>
2013-09-29 18:59:05 +0300

    image retrieval support

--- app/routes.php
+++ app/routes.php
@@ -1,19 +1,29 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+ |--------------------------------------------------------------------------
+ | Application Routes
+ |--------------------------------------------------------------------------
+ |
+ | Here is where you can register all of the routes for an application.
+ | It's a breeze. Simply tell Laravel the URIs it should respond to
+ | and give it the Closure to execute when that URI is requested.
+ |
+ */
 
-Route::get('/', function()
-{
+Route::get('/', function() {
 	return View::make('hello');
+});
+
+/**
+ * Image retriever
+ */
+Route::get('image.php/{id}', function($id) {
+	$image = Image::findOrFail($id);
+
+	$response = Response::make($image -> image, 200, array('Content-Type' => $image -> mime));
+
+	return $response;
 });
 
 Route::controller('cards', 'CardController');
