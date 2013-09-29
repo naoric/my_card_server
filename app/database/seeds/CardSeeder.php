71782a8b4edc16368d6bbf9936ece730a67dc30c
Naor Ami <naoric@gmail.com>
2013-09-29 18:40:15 +0300

    first db seed for cards

--- app/database/seeds/CardSeeder.php
+++ app/database/seeds/CardSeeder.php
@@ -3,12 +3,17 @@
 class CardSeeder extends Seeder {
 	public function run() {
 		DB::table('cards')->delete();
-		
+		DB::table('images')->delete();
+
+		$image = Image::create(array(
+			'image' => file_get_contents(app_path() . '/data/hamashbir.jpg'),
+			'mime' => 'image/jpeg'
+		));
+
 		Card::create(array(
-			'name' => 'Ha Mashbir la Zarhan',
-			'address' => 'Jabotinsky 14',
-			'description' => 'Shithole',
-			'image' => file_get_contents(app_path() . '/data/hamashbir.jpg')
+			'name' 		=> 'Ha Mashbir la Zarhan',
+			'description' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae nobis officia modi voluptate quia dignissimos odit dicta sequi voluptas reprehenderit quod quas voluptatibus quisquam mollitia architecto incidunt harum perferendis minus.',
+			'image_id' => $image->id
 		));
 		
 		
